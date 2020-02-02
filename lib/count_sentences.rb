50321048ec6c33ad4ce861499221be9812d6e0d8
David Ricketts <david.ricketts119@gmail.com>
2020-02-02 13:10:24 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,37 @@
 require 'pry'
 
+
+
+#!!!!!!move away for ide 
+#
+
+
 class String
 
   def sentence?
-    
+    return self[-1] == "."
   end
 
   def question?
-
+     return self[-1] == "?"
   end
 
   def exclamation?
-
+    return self[-1] == "!"
   end
 
   def count_sentences
-
+    
+    counter = 0 
+    letters = self.split('')
+    letters.each do | letter |
+      
+    if letter == "." || letter == "!" || letter == "?" 
+        counter += 1
+      end
+    end
+       
+    return counter 
+  
   end
 end
